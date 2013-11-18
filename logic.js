2d9730a961abbb04d3a716e05e974352db429607
Jeremy Dye <dyeje@mail.gvsu.edu>
2013-11-18 14:19:46 -0500

    Fixed JS.

--- logic.js
+++ logic.js
@@ -30,7 +30,7 @@ $(document).ready(function() {
         }
       }
     }
-    $('table.order tbody').html(html);
+    $('table.order').html(html);
     $('.orderTotal').html(total.toFixed(2));
   };
 
