cae6c3b01a56743e664854fffdb8a833ce6ac9b6
Jeremy Dye <dyeje@mail.gvsu.edu>
2013-11-19 20:41:44 -0500

    Getting shit done.

--- logic.js
+++ logic.js
@@ -1,8 +1,9 @@
 $(document).ready(function() {
+  var container = $(".container");
   var buttonContainer = $(".buttonContainer");
 
   //should be main menu later
-  buttonContainer.html($(".buttonDiv.top").html());
+  buttonContainer.html($(".buttonDiv.mainMenu").html());
 
   function MenuItem(id, options) {
     this.id = id;
@@ -25,8 +26,8 @@ $(document).ready(function() {
         "<td class='price'>$ " + menuItems[i].price + "</td></tr>";
 
       if (menuItems[i].condiments.length > 0) {
-        for (var i = 0; i < menuItems[i].condiments.length; i++) {
-          html += "<tr><td class='number'></td><td class='name'>" + menuItems[i].condiments[i] + "</td><td class='price'></td></tr>";
+        for (var j = 0; j < menuItems[i].condiments.length; j++) {
+          html += "<tr><td class='number'></td><td class='name'>" + menuItems[i].condiments[j] + "</td><td class='price'></td></tr>";
         }
       }
     }
@@ -34,20 +35,34 @@ $(document).ready(function() {
     $('.orderTotal').html(total.toFixed(2));
   };
 
-  buttonContainer.on("click", 'section.menuItemButton', function (e) {
+  container.on("click", '.menuItemButton', function (e) {
     e.preventDefault();
     var menuItemAttributes = $(this).data('attributes');
     if(menuItemAttributes) {
-      if (!menuItemAttributes.isCondiment) {
-        currentMenuItem.condiments.push(menuItemAttributes.name);
+      if (menuItemAttributes.isCondiment === "true") {
+        var input = $(this).find("input");
+        if (input.length > 0) {
+          if (input.prop("checked")) {
+            var index = currentMenuItem.condiments.indexOf(menuItemAttributes.name);
+            currentMenuItem.condiments.splice(index, 1);
+            input.prop("checked", false);
+
+          } else {
+            currentMenuItem.condiments.push(menuItemAttributes.name);
+            input.prop("checked", true);
+          }
+        } else {
+          currentMenuItem.condiments.push(menuItemAttributes.name);
+        }
       } else {
-        var menuItem = menuItems.push(new MenuItem(menuItems.length, menuItemAttributes));
-        if (!menuItemAttributes.hasCondiments) currentMenuItem = menuItem;
+        var menuItem = new MenuItem(menuItems.length, menuItemAttributes);
+        menuItems.push(menuItem);
+        if (menuItemAttributes.hasCondiments === "true") currentMenuItem = menuItem;
       }
     }
-
-    var next = ($(this).data("next") || $(this).closest(".buttonDiv").data("next"));
+    var next = ($(this).data("next") || $(this).closest(".nextDiv").data("next"));
     if (next) buttonContainer.html($(".buttonDiv." + next).html());
+    if (next === "mainMenu") currentMenuItem = null;
 
     rebuildOrder();
   });
